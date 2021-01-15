7964dbe64ff21f8d68e3b85f29dcb3ab072e2234
Krrish-shah <76941355+Krrish-shah@users.noreply.github.com>
2021-01-15 14:53:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var fixedRectangle,movingRectangle
+
+function setup() {
+  createCanvas(800,400);
+ fixedRectangle=createSprite(400, 200, 50, 80);
+fixedRectangle.shapeColor="green"
+movingRectangle= createSprite(400,200,80,50)
+movingRectangle.shapeColor="green"
+}
+
+function draw() {
+  background(255,255,255);
+  movingRectangle.x=World.mouseX
+  movingRectangle.y=World.mouseY  
+  console.log(movingRectangle.x-fixedRectangle.x)
+  if((movingRectangle.x-fixedRectangle.x<fixedRectangle.width/2+movingRectangle.width/2)&&
+  (fixedRectangle.x-movingRectangle.x<fixedRectangle.width/2+movingRectangle.width/2)&&
+  (fixedRectangle.y-movingRectangle.y<fixedRectangle.height/2+movingRectangle.height/2)&&
+  (movingRectangle.y-fixedRectangle.y<fixedRectangle.height/2+movingRectangle.height/2))
+
+  {
+    fixedRectangle.shapeColor="red"
+    movingRectangle.shapeColor="red"
+  }
+  else{fixedRectangle.shapeColor="green"
+movingRectangle.shapeColor="green"}
+
+  drawSprites();
+}
